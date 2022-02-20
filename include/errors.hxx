4b839b12a7f80126fd7d0de01b39049b2571d16e
Stefan Olivier <snowflake1194@gmail.com>
2022-02-20 23:09:52 +0200

    added locale, io, error headers added edlfile headers with basic scaffolding for edl file parsing

---
+++ include/errors.hxx
@@ -0,0 +1,9 @@
+// Stefan Olivier
+// Description: Error handling
+
+# pragma once
+
+namespace ststd::errors
+{
+    using ERROR_TYPE = int;
+}
---
+++ include/locale.hxx
@@ -0,0 +1,14 @@
+// Stefan Olivier
+// Description: Locale defintions and configuration
+
+#pragma once
+
+#include <string>
+#include <string_view>
+
+namespace sostd::locale
+{
+    using STRING_TYPE      = std::string;
+    using STRING_VIEW_TYPE = std::string_view;
+}
+
