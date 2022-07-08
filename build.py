4d902d116595d0693bea73416d567cdea4070467
Stefan Olivier <snowflake1194@gmail.com>
2022-07-08 15:10:50 +0200

    WIP build.py & settings.json

--- build.py
+++ build.py
@@ -11,11 +11,11 @@ def replace_macro(raw_paths):
     tokens = {
         'WORKSPACE': os.path.dirname(os.path.realpath(__file__))
     }
-
+    
     for k1,v1 in raw_paths.items():
         for k2,v2 in tokens.items():
             p = '\\$\\{' + k2 + '\\}'
-            paths[k1] = os.path.abspath(re.sub(pattern=p, repl=v2, string=v1))
+            paths[k1] = re.sub(string=v1, pattern=p, repl=v2.replace('\\', '/'))
 
     return paths
 
@@ -28,8 +28,8 @@ def main():
                         help='which build command to execute, specified in compile settings.json')
     parser.add_argument('--target', metavar='str', type=str, nargs='?', default='ALL_BUILD',
                         help='cmake build target, default="ALL_BUILD"')
-    parser.add_argument('--settings', metavar='s', type=str, nargs='?', default='"./settings.json:"',
-                        help='location of project configuration file. default=./settings.json')
+    parser.add_argument('--settings', metavar='s', type=str, nargs='?', default='./settings.json',
+                        help='location of project configuration file. default="./settings.json"')
     args = parser.parse_args()
 
     # Paths
@@ -39,44 +39,49 @@ def main():
     system = 'unknown'
     if (platform.system() == 'Darwin'):
         system = 'macosx'
-    elif (platform.system() == 'Win32'):
+    elif (platform.system() == 'Windows'):
         system = 'win32'
     elif (platform.system() == 'Linux'):
         system = 'linux'
 
-    settings = json.load(open('settings.json', 'r'))
-    cmake_cfg = settings['cmake'][system]['configure']
+    settings = json.load(open(args.settings, 'r'))
     paths = replace_macro(settings['global'])
+    cmake_cfg = settings['cmake'][system]['configure']
 
-    if ('configure' in args.action):
+    cfg_entries = [x for x in cmake_cfg if x['name'] == args.cfg]
+    if (len(cfg_entries) > 1):
+        raise f'duplicate configure in {args.settings} items with name {args.cfg}'
+
+    elif (len(cfg_entries) == 1):
         cmd = 'cmake'
+        cfg = cfg_entries[0]
 
-        flags = cmake_cfg['flags']
+        flags = cfg['flags']
         for f in flags:
             cmd += f' {f}'
 
-        defs = cmake_cfg['definitions']
+        defs = cfg['definitions']
         for d in defs:
             n = d['name']
             t = d['type']
             v = d['value']
             cmd += f' -D{n}:{t}={v}'
 
-        defs = cmake_cfg['toolset']
+        defs = cfg['toolset']
         for d in defs:
             n = d['name']
             v = d['value']
             cmd += f' -T {n}={v}'
 
-        gen = cmake_cfg['generator']
+        gen = cfg['generator']
         if (gen != 'default'): cmd += f' -G {gen}'
 
-        cmd += f' {cmake_cfg["arch"]}'
+        cmd += f' {cfg["arch"]}'
         cmd += f' -B {paths["build_path"]}'
         cmd += f' -S {cwd}'
 
-        print(cmd)
-        os.system(cmd)
+        print(f'executing expression: {cmd}')
+        # os.system(cmd)
 
 if __name__ == '__main__':
     main()
